9cd5c71c6d4361f8c9a8dd72d4cec6bf99f7005c
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-08-11 09:04:40 -0700

    include gitignore files and initial setup

---
+++ src/main/java/model/Event.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by Guest on 8/11/17.
+ */
+public class Event {
+
+}
---
+++ src/test/java/model/EventTest.java
@@ -0,0 +1,20 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/11/17.
+ */
+public class EventTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
