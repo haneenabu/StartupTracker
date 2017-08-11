c7d89f6753e8530fc3afc310e0fefb052ce481e0
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-08-11 10:09:34 -0700

    include functionality to cause test2 to pass

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -4,8 +4,22 @@ package model;
  * Created by Guest on 8/11/17.
  */
 public class Event {
+    private String name;
+    private String description;
+
+
+
     //constructor
     public Event (String name, String description){
+        this.name = name;
+        this.description = description;
+    }
 
+    //getters
+    public String getName(){
+        return name;
+    }
+    public String getDescription() {
+        return description;
     }
 }
